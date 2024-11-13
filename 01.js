c69d288a5116deb4fce3d990e04c4ff24d2b68e0
krish-1612 <tkrish062@gmail.com>
2024-11-13 16:47:05 +0530

    01.js and datatypes.js

---
+++ 01.js
@@ -0,0 +1,21 @@
+// structured programming Language
+// scripting language
+
+// print statement 
+
+console.log("Namste World")
+
+// variable
+
+// let, const, var
+
+let age = 20
+const price = 252.23
+var name = "krish"
+
+console.log(age)
+console.log(typeof(age))
+console.log(price)
+console.log(typeof(price))
+console.log(name)
+console.log(typeof(name))
