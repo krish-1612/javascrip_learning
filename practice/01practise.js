c5a7a0b29cef5757725bfccc931722e62279ac87
krish-1612 <tkrish062@gmail.com>
2024-11-20 14:40:56 +0530

    cal,dt,datatype,01

---
+++ practice/01practise.js
@@ -0,0 +1,10 @@
+let amount = 300
+const valueprice = 501.23
+var name = "5star"
+
+console.log(amount);
+console.log(valueprice);
+console.log(name);
+
+
+ 
---
+++ practice/DT_practice.js
@@ -0,0 +1,37 @@
+let myname = "krish"
+
+let todayiswednesday = false
+
+let myage = 19
+
+let price = null
+
+let anyperson 
+
+let railway = ["rajdhani","memu","gujarat_exp",654,52.36,true]
+let persondetails = {
+    name: "sant",
+    age : 45,
+    city : "kashi",
+}
+
+console.log(myname);
+console.log(typeof(myname));
+console.log(todayiswednesday);
+console.log(typeof(todayiswednesday));
+console.log(myage);
+console.log(typeof(myage));
+console.log(price);
+console.log(typeof(price));
+console.log(anyperson);
+console.log(typeof(anyperson));
+console.log(railway);
+console.log(typeof(railway));
+console.log(persondetails);
+console.log(typeof(persondetails));
+
+
+
+
+
+
